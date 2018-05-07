fce001b80fa32a28820aac390fb07157937c2a07
Tad <t@t.com>
2018-05-06 21:30:58 -0400

    asdf

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 var restify = require('restify');
 var builder = require('botbuilder');
+var azure = require('botbuilder-azure');
+
+var documentDbOptions = {
+    host: 'https://charliebotcosmosdb.documents.azure.com:443/',
+    masterKey: '2DlKR2cb1xT3fjVb2zm79rz0A5IlHxJzGjB91UN60gtC5q0fY4lbaST9Ge3L5K7F0k45HnQb0DLMlJL2iOtVUA==',
+    database: 'charliebotcosmosdb',
+    collection: 'bot'
+};
+
+var docDbClient = new azure.DocumentDbClient(documentDbOptions);
+
+var cosmosStorage = new azure.AzureBotStorage({ gzipData: false }, docDbClient);
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -17,6 +29,11 @@ var connector = new builder.ChatConnector({
 server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+// var bot = new builder.UniversalBot(connector, function (session) {
+//     session.send("You said: %s", session.message.text);
+// });
+
 var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
-});
+  session.send("You said: %s", session.message.text);
+})
+.set('storage', cosmosStorage);
